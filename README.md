134b3f71d444be93abf4d5d25c6657a5f3a03f96
Andrew Paster <apaster@MacBook-Pro-4.local>
2018-06-21 12:11:30 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# dsnd-distributions package
+
+Summary of the package
+
+# Files
+
+Explanation of files in the package
+
+# Installation
+
+How to install the package
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability'],
+      author = 'Andrew Paster',
+      author_email = 'myemail@something.com',
+    zip_safe=False)
